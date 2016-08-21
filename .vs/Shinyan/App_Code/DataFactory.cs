0c2a317446a7027430742185cb751c81dbec1361
rugia <rugia813@yahoo.com.tw>
2016-08-21 20:00:00 +0800

    -sidebar width, 4 pages from leo

---
+++ .vs/Shinyan/App_Code/DataFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// DataFactory 的摘要描述
+/// </summary>
+public class DataFactory
+{
+    public DataFactory()
+    {
+        //
+        // TODO: 在這裡新增建構函式邏輯
+        //
+    }
+}
